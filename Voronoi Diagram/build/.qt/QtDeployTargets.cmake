c7643e1ad73a472ad9f53c0083cfc8fab59052af
anjalid011 <anjalidongare011@gmail.com>
2025-04-24 18:34:42 +0530

    updated

--- Voronoi Diagram/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VoronoiApp_FILE C:/Users/Anjali Dongare/Desktop/Project/Cctech/Voronoi Diagram/build/VoronoiApp.exe)
-set(__QT_DEPLOY_TARGET_VoronoiApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VoronoiApp_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
